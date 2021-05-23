5e837d52915a88e1b0869330a44551d6aba94add
Aga Bie <abiesiadka@gmail.com>
2021-05-23 16:15:56 +0100

    [fix] - correct data.sql file created

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ This is my first budget project based on Java and Spring.
 
 - przed mergem/pull/przelaczeniem drzewo robocze powinno byc czyste
 - usuwamy tylko branch na ktorym aktualnie sie nie znajdujemy
-
+- po mergu zawsze zweryfikujmy czy wszyskto działa (ale tylko jeśli musimy rozwiązywac konflikty)
+- zmergowany feature-branch można bezpiecznie usunąć
 ### Git - przydatne komendy
 git status - co sie dzieje w repo
 git log - historia
@@ -12,5 +13,15 @@ git log --graph - rysuje graf historii
 git add .-podaje do stage`a wszystkie zmienione albo nowe pliki. wtedy mozna commit
 git commit -m "co sie zmienia" zapis zmian do repozytorium
 git branch -a - wyswietl wszystkie branche
+git checkout nazwa_brancha - przełączenia na inny branch
+git checkout -b nazwa_brancha - przełączenie na właśnie tworzony/nowy branch
+git push nazwa-repo nazwa-brancha np. git push origin master - wypchnięcie zmian do nazwa-repo z brancha nazwa-brancha
+git remote - pokaż zdalne repozytoria
+git remote -v - jw. + adresy
 
+
+### MVC pattern
+Controller
+Service
+Repository
 
--- src/main/java/pl/sda/mybudget/controller/rest/IncomeRestController.java
+++ src/main/java/pl/sda/mybudget/controller/rest/IncomeRestController.java
@@ -1,25 +1,31 @@
 package pl.sda.mybudget.controller.rest;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sda.mybudget.model.Income;
 import pl.sda.mybudget.model.enumeration.IncomeType;
+import pl.sda.mybudget.service.IncomeService;
 
 import java.time.LocalDate;
 import java.util.List;
-
 @RestController
 @RequestMapping("/rest/incomes")
 public class IncomeRestController {
 
-    @GetMapping
-    List<Income> getAllIncomes() {
-        return List.of(
-                new Income(1_000, LocalDate.now(), "Od żony:)", IncomeType.MOPS),
-                new Income(5_00_000, LocalDate.now(), "500++", IncomeType.MOPS)
-        );
+    private final IncomeService incomeService;
+
+    public IncomeRestController(final IncomeService incomeService) {
+        this.incomeService = incomeService;
     }
 
 
+
+    @GetMapping
+    List<Income> getAllIncomes() {
+
+            return incomeService.findAllIncomes();
 }
+
+
+}
+
